402a60d8821f970cc489233ef179a6b3cafb1f65
Umut ISIK <uuisik@gmail.com>
2021-04-26 18:41:37 +0300

    service added

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const r1 = async (address) => {
     if (data) {
       const flag = 1 * data.balance + 1 * data.total_received + 1 * data.total_send + 1 * data.tx_count;
       if (flag === 0) {
-        console.warn(address + " is empty");
+        // console.warn(address + " is empty");
         return false;
       } else {
         console.log("BINGO !!!");
@@ -49,25 +49,25 @@ const r1 = async (address) => {
   return false;
 };
 
-const f2 = () => {
-  // private:
-  // KxKUvegJjKdi3BfvGDmRBiXpgmjz4CDDhfg7qXzv3SHB8FGaGQCs
-  // public:
-  // 17PyydCeABK3A1rydYPN6DyAzA7WcMPtEe
+// const f2 = () => {
+//   // private:
+//   // KxKUvegJjKdi3BfvGDmRBiXpgmjz4CDDhfg7qXzv3SHB8FGaGQCs
+//   // public:
+//   // 17PyydCeABK3A1rydYPN6DyAzA7WcMPtEe
 
-  const keyPair = bitcoin.ECPair.fromWIF("KxKUvegJjKdi3BfvGDmRBiXpgmjz4CDDhfg7qXzv3SHB8FGaGQCs");
-  console.log(keyPair.toWIF());
+//   const keyPair = bitcoin.ECPair.fromWIF("KxKUvegJjKdi3BfvGDmRBiXpgmjz4CDDhfg7qXzv3SHB8FGaGQCs");
+//   console.log(keyPair.toWIF());
 
-  const p2pkh = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
-  console.log(p2pkh.pubkey.toWIF());
-};
+//   const p2pkh = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+//   console.log(p2pkh.pubkey.toWIF());
+// };
 
-const f3 = () => {
-  const private_key = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";
-  const decoded_private_key = bitcoin.decode_privkey(private_key, "hex");
-  const wif = bitcoin.encode_privkey(decoded_private_key, "wif");
-  console.log(private_key, decoded_private_key, wif);
-};
+// const f3 = () => {
+//   const private_key = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";
+//   const decoded_private_key = bitcoin.decode_privkey(private_key, "hex");
+//   const wif = bitcoin.encode_privkey(decoded_private_key, "wif");
+//   console.log(private_key, decoded_private_key, wif);
+// };
 
 const run = async () => {
   // console.log("started");
